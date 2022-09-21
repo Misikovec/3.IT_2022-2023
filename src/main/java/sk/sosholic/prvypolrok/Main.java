0684e3311be17e7609885a7437e044a4da677865
Misik <andrej.misik@sosholic.sk>
2022-09-21 08:36:43 +0200

    Initial commit

---
+++ src/main/java/sk/sosholic/prvypolrok/Main.java
@@ -0,0 +1,4 @@
+package sk.sosholic.prvypolrok;
+
+public class Main {
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/opakovanie.java
@@ -0,0 +1,10 @@
+package sk.sosholic.prvypolrok;
+
+public class opakovanie {
+    public static void main(String[] args) {
+
+        System.out.println("Bol som v Tatrách a hned na to v Chorvatsku ");
+        System.out.println("Robil som autoškolu");
+        System.out.println("Chodil som s kamaratmi von");
+    }
+}
